0835ab2cc5e43ec997ff673f68f70e403df372b4
AscencioSIUU <131807749+AscencioSIUU@users.noreply.github.com>
2024-08-23 07:18:59 -0600

    ComposeUpdated

--- app/src/main/java/com/example/calculatorusingcomposable/MainActivity.kt
+++ app/src/main/java/com/example/calculatorusingcomposable/MainActivity.kt
@@ -1,23 +1,30 @@
 package com.example.calculatorusingcomposable
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.calculatorusingcomposable.ui.theme.CalculatorUsingComposableTheme
-import javax.xml.xpath.XPathExpression
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,77 +32,151 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CalculatorUsingComposableTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    Calculator()
                 }
             }
         }
     }
 }
-
+@Preview(showBackground = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Calculator(
+    modifier: Modifier = Modifier
+) {
+    var title by remember { mutableStateOf("")}
+    var subtitle by remember { mutableStateOf("")}
+    val placeholder = "0"
+
+    Column {
+        Display(title,subtitle,placeholder)
+        ButtonLists(
+            onButtonClick = {value ->
+                title += value
+            }
+        )
+    }
+
 }
 
 @Composable
-fun Display(inputInfix: String){
-    Text(
-        text = "$inputInfix"
-    )
+fun Display(title: String, subtitle: String, placeholder: String){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.End
+    ){
+        Text(
+            text = if(title.isEmpty()) placeholder else title,
+            style = MaterialTheme.typography.titleLarge,
+            textAlign = TextAlign.End
+        )
+        Text(
+            text = if(subtitle.isEmpty()) placeholder else subtitle,
+            style = MaterialTheme.typography.titleMedium,
+            textAlign = TextAlign.End
+        )
+    }
+    }
 }
+
 
 @Composable
 fun ItemButton(
     symbol: String,
     onContinueClicked: () -> Unit,
+    color: Color,
     modifier: Modifier = Modifier
 ){
     Button(
         onClick = onContinueClicked,
-        modifier = modifier.padding(vertical = 4.dp, horizontal = 4.dp)
+
+        modifier = modifier
+            .padding(vertical = 4.dp, horizontal = 4.dp)
     ) {
         Text("$symbol")
     }
 }
 
-
-
-@Preview(showBackground = true)
 @Composable
 fun ButtonRows(
-    itemsPerColumn: List<String> = List(4) { "$it" },
+    buttons: List<CalculatorButtonData>,
+    onButtonClick: (String) -> Unit,
     modifier: Modifier = Modifier
 ){
     Row (
-        verticalAlignment = Alignment.CenterVertically
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier.fillMaxWidth()
     ) {
-       for (i in 0..4){
-           ItemButton("Siu", onContinueClicked = {})
+       buttons.forEach{ button ->
+           ItemButton(
+               symbol = button.buttonText ,
+               onContinueClicked = { onButtonClick(button.onClickValue) })
        }
+    }
+}
+
+@Composable
+fun ButtonLists (
+    onButtonClick: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var buttonGroups = calculatorButtons.chunked(4)
+    Column {
+        buttonGroups.forEach{ group ->
+            ButtonRows(
+                buttons = group,
+                onButtonClick = onButtonClick,
+                color
+            )
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun ButtonLists (
-    modifier: Modifier = Modifier
-) {
-    Column (){
-
-    }
+fun PreviewDisplay(){
+    Display(title = "", subtitle = "", placeholder = "0")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CalculatorUsingComposableTheme {
-        Greeting("Android")
+        Calculator()
     }
 }
+
+data class CalculatorButtonData(val buttonText: String, val onClickValue: String)
+private val calculatorButtons = listOf(
+    CalculatorButtonData("C", "C"),
+    CalculatorButtonData("√", "√"),
+    CalculatorButtonData("π", "π"),
+    CalculatorButtonData("÷", "÷"),
+
+    CalculatorButtonData("7", "7"),
+    CalculatorButtonData("8", "8"),
+    CalculatorButtonData("9", "9"),
+    CalculatorButtonData("*", "*"),
+
+    CalculatorButtonData("4", "4"),
+    CalculatorButtonData("5", "5"),
+    CalculatorButtonData("6", "6"),
+    CalculatorButtonData("-", "-"),
+
+    CalculatorButtonData("1", "1"),
+    CalculatorButtonData("2", "2"),
+    CalculatorButtonData("3", "3"),
+    CalculatorButtonData("+", "+"),
+
+    CalculatorButtonData(".", "."),
+    CalculatorButtonData("0", "0"),
+    CalculatorButtonData("=", "=")
+)
